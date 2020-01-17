79cccaee544785a45e6e33428da1a1d21b040e83
James DeVore <jdevore4592@gmail.com>
2020-01-16 21:17:10 -0500

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 //  Author: James DeVore
 //  why? to learn rust 
 
+extern crate chrono;
+
 use std::io;
+use ::chrono::prelude::*;
 
 fn main() {
     let name = input("What is your name? ").expect("Something failed");
 
     let age = input("What is your age? ")
     .expect("Not a number or failed")
-    .parse::<u8>().expect("Invalid");
+    .parse::<i32>().expect("Invalid");
 
-    println!("{} is {} years old", name, age);
+    println!("{} is {} years old", &name, &age);
+
+    let current_year = Utc::now().year() ;
+    let year_100 = 100 - age + current_year;
+    println!("{} will be 100 in the year {}", name, year_100)
 }
 
 /// `input` mimics the input function for python
